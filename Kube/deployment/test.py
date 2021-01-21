a78fb8e461098709f333243961093420c5918f48
aamerm <aamer@usa.com>
2021-01-21 13:39:41 +0530

    Updated

---
+++ Kube/deployment/test.py
@@ -0,0 +1,9 @@
+import os
+import sys
+firstname = input('Please provide your firstname: ')
+lastname = input('Please provide your lastname: ')
+
+if firstname == 'Aamer':
+    print('Hey.. I know you.')
+else:
+    print('Hello', firstname)
